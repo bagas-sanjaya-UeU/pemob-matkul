99fbf4744b945301e0f913cae2c4c812323cf27e
bagas-sanjaya-UeU <charderrasanjaya@student.esaunggul.ac.id>
2025-01-01 16:59:23 +0000

    enable forced HTTPS scheme in AppServiceProvider

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -24,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Activity::class, ActivityPolicy::class);
+        \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
